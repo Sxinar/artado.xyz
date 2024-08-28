5da1584762827abf1e7a768a21ab687020e4c7fb
Sxinar <130384567+Sxinar@users.noreply.github.com>
2024-08-28 21:22:13 +0300

    Add files via upload

--- assest/js/script.js
+++ assest/js/script.js
@@ -11,15 +11,48 @@ document.addEventListener('DOMContentLoaded', function() {
         sideMenu.style.width = '0';
     });
 
-    // Dropdown işlevselliği için
-    var dropdowns = document.querySelectorAll('.dropdown');
-    dropdowns.forEach(function(dropdown) {
-        dropdown.addEventListener('click', function() {
-            this.classList.toggle('active');
-            var icon = this.querySelector('.dropdown-icon');
-            icon.textContent = this.classList.contains('active') ? '▲' : '▼';
-        });
+    var pagesDropdown = document.getElementById('pagesDropdown');
+    var socialsDropdown = document.getElementById('socialsDropdown');
+    var pagesPopup = document.getElementById('pagesPopup');
+    var socialsPopup = document.getElementById('socialsPopup');
+    var pagesContainer = document.getElementById('pagesContainer');
+
+    pagesDropdown.addEventListener('click', function(event) {
+        event.stopPropagation();
+        pagesPopup.classList.toggle('show');
+        pagesContainer.classList.toggle('active');
+        socialsPopup.classList.remove('show');
+        toggleIcon(pagesDropdown);
+        resetIcon(socialsDropdown);
     });
+
+    socialsDropdown.addEventListener('click', function(event) {
+        event.stopPropagation();
+        socialsPopup.classList.toggle('show');
+        pagesPopup.classList.remove('show');
+        pagesContainer.classList.remove('active');
+        toggleIcon(socialsDropdown);
+        resetIcon(pagesDropdown);
+    });
+
+    // Sayfa herhangi bir yerine tıklandığında popup'ları kapat
+    document.addEventListener('click', function(event) {
+        if (!event.target.closest('.dropdown-container')) {
+            pagesPopup.classList.remove('show');
+            socialsPopup.classList.remove('show');
+            pagesContainer.classList.remove('active');
+            resetIcon(pagesDropdown);
+            resetIcon(socialsDropdown);
+        }
+    });
+
+    function toggleIcon(dropdown) {
+        var icon = dropdown.querySelector('.dropdown-icon');
+        icon.style.transform = icon.style.transform === 'rotate(180deg)' ? 'rotate(0deg)' : 'rotate(180deg)';
+    }
+
+    function resetIcon(dropdown) {
+        var icon = dropdown.querySelector('.dropdown-icon');
+        icon.style.transform = 'rotate(0deg)';
+    }
 });
-
-
