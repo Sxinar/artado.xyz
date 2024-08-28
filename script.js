d2b5693a095810aa6ede3c26930df2ffbb9107e4
Sxinar <130384567+Sxinar@users.noreply.github.com>
2024-08-28 17:04:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
-const popupMenu = document.getElementById('popup-menu');
-let isDragging = false;
-let startX;
+document.addEventListener('DOMContentLoaded', function() {
+    var menuButton = document.getElementById('menuButton');
+    var closeButton = document.getElementById('closeButton');
+    var sideMenu = document.getElementById('sideMenu');
 
-document.getElementById('hamburger').addEventListener('click', function() {
-    popupMenu.classList.toggle('show');
+    menuButton.addEventListener('click', function() {
+        sideMenu.style.width = '250px';
+    });
+
+    closeButton.addEventListener('click', function() {
+        sideMenu.style.width = '0';
+    });
+
+    // Dropdown işlevselliği için
+    var dropdowns = document.querySelectorAll('.dropdown');
+    dropdowns.forEach(function(dropdown) {
+        dropdown.addEventListener('click', function() {
+            this.classList.toggle('active');
+            var icon = this.querySelector('.dropdown-icon');
+            icon.textContent = this.classList.contains('active') ? '▲' : '▼';
+        });
+    });
 });
 
-popupMenu.addEventListener('touchstart', function(e) {
-    startX = e.touches[0].clientX;
-    isDragging = true;
-});
 
-popupMenu.addEventListener('touchmove', function(e) {
-    if (!isDragging) return;
-    const touchX = e.touches[0].clientX;
-    const distance = touchX - startX;
-
-    if (distance > 50) {  // Eğer sağa kaydırma 50 pikselden fazlaysa
-        popupMenu.style.transform = `translateX(${distance}px)`;
-    }
-});
-
-popupMenu.addEventListener('touchend', function(e) {
-    if (!isDragging) return;
-    isDragging = false;
-    const touchX = e.changedTouches[0].clientX;
-    const distance = touchX - startX;
-
-    if (distance > 100) {  // Sağa kaydırma 100 pikselden fazlaysa menüyü kapat
-        popupMenu.classList.remove('show');
-    } else {
-        popupMenu.style.transform = 'translateX(0)';
-    }
-});
